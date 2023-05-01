b0864fc55d3ac81e1feb616a83508180ceca9333
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-01 15:02:27 +0200

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Eric on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
