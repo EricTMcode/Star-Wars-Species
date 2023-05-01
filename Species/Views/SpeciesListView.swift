d15323312f7d102acbe000f62209fcaf5d124d73
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-01 15:08:27 +0200

    - Create the project - Add the assets, the app icon and the launchscreen - Create the folder

--- Species/Views/SpeciesListView.swift
+++ Species/Views/SpeciesListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SpeciesListView.swift
 //  Species
 //
 //  Created by Eric on 01/05/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SpeciesListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SpeciesListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SpeciesListView()
     }
 }
